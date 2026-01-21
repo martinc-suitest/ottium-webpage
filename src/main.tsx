7ed929e99102c72c017d9db4372d832b33c73036
unknown <martin.c@suite.st>
2026-01-21 17:42:58 +0100

    adaptations for smaller screens

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import { setSelectedCategory } from './platformsSwitchHandler';
 // Ensure setSelectedCategory is available globally for inline HTML usage
 // @ts-ignore
 window.setSelectedCategory = setSelectedCategory;
+import './styles/platforms.css'
 import './styles/main.css'
+import './styles/footer.css'
 
 ReactDOM.createRoot(document.getElementById('navbar-root')!).render(
   <App />,
